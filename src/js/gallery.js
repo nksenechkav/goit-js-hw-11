b56b5f1bcf1b5c7891df53cbbb59a48542129dac
nksenechkav <144259943+nksenechkav@users.noreply.github.com>
2024-02-09 11:59:30 +0100

    add js files and libraries

---
+++ src/js/gallery.js
@@ -0,0 +1,109 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+// const refs = {
+//     formEl: document.querySelector('.js-search-form[data-id="js-search-image"]'),
+//     imageEl: document.querySelector('.js-image-container'),
+//   };
+  
+//   refs.formEl.addEventListener('submit', e => {
+//     e.preventDefault();
+  
+//     const image = e.target.elements.query.value;
+  
+//     searchImage(image).then(data => {
+//       renderImage(data);
+//     });
+  
+//     e.target.reset();
+//   });
+  
+//   function searchImage(userValue) {
+//     const BASE_URL = 'https://pixabay.com';
+//     const END_POINT = '/api/';
+//     const PARAMS = `?key=${userValue}`;
+//     const url = BASE_URL + END_POINT + PARAMS;
+  
+//     const options = {
+//       headers: {
+//         'X-RapidAPI-Key': '42271393-ceafa19bde7d0a63fb15d5d6f',
+//         'X-RapidAPI-Host': 'superhero-search.p.rapidapi.com',
+//       },
+//     };
+  
+//     return fetch(url, options).then(res => res.json());
+//   }
+  
+//   function imageTemplate(image) {
+//     return `<div class="image-card card">
+//     <div class="image-container">
+//       <img
+//         src="${image.images.lg}"
+//         alt="#"
+//         class="hero-image"
+//       />
+//     </div>
+//     <div class="image-body">
+//       <h4 class="image-name">${image.name}</h4>
+//       <p class="image-bio">
+//         ${image.biography.fullName} - ${image.biography.placeOfBirth}, ${image.work.base}
+//       </p>
+//     </div>
+//   </div>`;
+//   }
+  
+//   function renderImage(image) {
+//     const markup = imageTemplate(image);
+//     refs.imageEl.insertAdjacentHTML('afterbegin', markup);
+//   }
+const form = document.querySelector('.form-control');
+const loader = document.querySelector('.loader');
+const gallery = document.querySelector('.gallery');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const keyword = document.querySelector('#keyword').value;
+  const query = `https://pixabay.com/api/?key={42271393-ceafa19bde7d0a63fb15d5d6f}&q=${keyword}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+  loader.style.display = 'block';
+  gallery.innerHTML = '';
+
+  fetch(query)
+    .then(response => response.json())
+    .then((data) => {
+      loader.style.display = 'none';
+      if (data.hits.length === 0) {
+        gallery.innerHTML = 'No results found.';
+        return;
+      }
+
+      data.hits.forEach((hit) => {
+        const image = document.createElement('img');
+        image.src = hit.webformatURL;
+        image.alt = hit.tags;
+        image.addEventListener('click', () => {
+          const lightbox = new SimpleLightbox();
+          lightbox.open({
+            items: [
+              {
+                src: hit.largeImageURL,
+                caption: `Likes: ${hit.likes}, Views: ${hit.views}, Comments: ${hit.comments}, Downloads: ${hit.downloads}`
+              }
+            ]
+          });
+        });
+        gallery.appendChild(image);
+      });
+
+      const lightbox = new SimpleLightbox();
+      lightbox.refresh();
+    })
+    .catch((error) => {
+      loader.style.display = 'none';
+      gallery.innerHTML = 'Error fetching images.';
+      console.error(error);
+    });
+});
